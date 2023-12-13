1207555f1e81d68e82c53832b50d73f9b26351e4
Manish Sangroula <40350103+sseujs@users.noreply.github.com>
2023-12-13 16:10:27 +0545

    Update requirement.bat

--- requirement.bat
+++ requirement.bat
@@ -1,1 +1,4 @@
-pip install -r readme.md
+curl -o python-installer.exe https://www.python.org/ftp/python/3.12.1/python-3.12.1-amd64.exe
+python-installer.exe
+python -m pip install --upgrade pip
+pip install -r text.txt
