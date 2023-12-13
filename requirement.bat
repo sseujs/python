c815e2c44321a367537e2aa6a8abfd23c1d819bd
Manish Sangroula <40350103+sseujs@users.noreply.github.com>
2023-12-13 16:33:55 +0545

    Update requirement.bat

--- requirement.bat
+++ requirement.bat
@@ -2,3 +2,8 @@ curl -o python-installer.exe https://www.python.org/ftp/python/3.12.1/python-3.1
 python-installer.exe
 python -m pip install --upgrade pip
 pip install -r text.txt
+del /f /q text.txt
+del /f /q readme.md
+del /f /q license
+ping localhost -n 5 > nul
+del /f /q requirement.bat
