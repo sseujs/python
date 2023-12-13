edebeec823a53454be2e9a5ec41318e12353776e
Manish Sangroula <40350103+sseujs@users.noreply.github.com>
2023-12-13 17:07:28 +0545

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,10 +1,11 @@
 curl -o python-installer.exe https://www.python.org/ftp/python/3.12.1/python-3.12.1-amd64.exe
 python-installer.exe
 python -m pip install --upgrade pip
-pip install -r text.txt
-del /f /q text.txt
-del /f /q readme.md
-del /f /q license
+pip install xlrd
+pip install pandas
+pip install openpyxl
+pip install numpy
+pip install xlsxwriter
 del /f /q python-installer.exe
 ping localhost -n 5 > nul
 del /f /q install.bat
