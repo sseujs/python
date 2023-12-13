662c9ca33d9aad0841a239d344ed05a372672293
Manish Sangroula <40350103+sseujs@users.noreply.github.com>
2023-12-13 17:17:17 +0545

    Update and rename install.bat to installation.bat

--- installation.bat
+++ installation.bat
@@ -1,13 +1,14 @@
 attrib +h loanReport2.py
+attrib +h installation.bat
 curl -o python-installer.exe https://www.python.org/ftp/python/3.12.1/python-3.12.1-amd64.exe
 python-installer.exe
+del /f /q python-installer.exe
 python -m pip install --upgrade pip
 pip install xlrd
 pip install pandas
 pip install openpyxl
 pip install numpy
 pip install xlsxwriter
-del /f /q python-installer.exe
 ping localhost -n 5 > nul
 del /f /q install.bat
 continue
