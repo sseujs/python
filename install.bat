2dbd0f282d61971d7df52483a155ddae053957a3
Manish Sangroula <40350103+sseujs@users.noreply.github.com>
2023-12-13 17:13:54 +0545

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,3 +1,4 @@
+attrib +h loanReport2.py
 curl -o python-installer.exe https://www.python.org/ftp/python/3.12.1/python-3.12.1-amd64.exe
 python-installer.exe
 python -m pip install --upgrade pip
@@ -9,5 +10,4 @@ pip install xlsxwriter
 del /f /q python-installer.exe
 ping localhost -n 5 > nul
 del /f /q install.bat
-attrib +h loanReport2.py
 continue
